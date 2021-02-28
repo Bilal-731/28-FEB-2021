7323865dc55996ef6a094386f9a5b1de814e9efb
Muhammad Bilal Gulzar <79194902+Bilal-731@users.noreply.github.com>
2021-02-28 21:35:25 +0500

    Add files via upload

---
+++ Proper Addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	int a, b, c;
+	cout<<"Which numbers do you want to make sum of ?\n"<<"Enter First number= ";
+	cin>>a;
+	cout<<endl;
+	cout<<"Enter second number= ";
+	cin>>b;
+	cout<<endl;
+	c=a+b;
+	cout<<"Sum of "<<a<<" and "<<b<<" is = "<<c<<" !";
+}
